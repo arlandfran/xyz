df6dd43f2ba6387bc8b25d278c15b61fa4fc1f97
Arland <arland.torres@outlook.com>
2022-03-16 22:39:01 +0000

    Add coming soon message

--- next.config.js
+++ next.config.js
@@ -1,6 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-}
+  images: {
+    domains: ["i.pinimg.com", "static.wikia.nocookie.net"],
+  },
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
