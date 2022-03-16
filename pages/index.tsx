7e670d7364205c92db4de8c6ec26544ddeae3dcc
Arland <arland.torres@outlook.com>
2022-03-16 23:06:57 +0000

    Refactor and use semantic figure tag

--- pages/index.tsx
+++ pages/index.tsx
@@ -4,17 +4,21 @@ import Image from "next/image";
 const Home: NextPage = () => {
   return (
     <>
-      <span className="text-white mb-2 tracking-[0.2rem] font-light">
-        pov: when the portfolio is coming soon&trade;
-      </span>
-      <Image
-        src={
-          "https://static.wikia.nocookie.net/3ba95285-8bb0-45eb-aeb6-419da3075335/"
-        }
-        alt="Crying cat meme"
-        width={300}
-        height={300}
-      />
+      <figure className="p-8">
+        <figcaption className="text-white tracking-[0.2rem] font-light mb-2">
+          pov:
+          <br />
+          when the portfolio is coming soon&trade;
+        </figcaption>
+        <Image
+          src={
+            "https://static.wikia.nocookie.net/3ba95285-8bb0-45eb-aeb6-419da3075335/"
+          }
+          alt="Crying cat meme"
+          width={345}
+          height={450}
+        />
+      </figure>
     </>
   );
 };
